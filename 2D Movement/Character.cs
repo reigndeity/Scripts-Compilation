551290e9db0bd61954a5ce26366e1b3c2f40b64a
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2024-08-01 02:12:51 +0800

    Add files via upload

---
+++ 2D Movement/Character.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveX;
+    public float moveY;
+    public float moveSpeed;
+    public bool isFacingRight;
+
+    void Update()
+    {
+        // Gets the input from unity
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY =  Input.GetAxisRaw("Vertical");
+
+        // Flips the sprite
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        // This makes it moves
+        rb.velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+    }
+
+    void Flip()
+    {
+        if (isFacingRight && moveX < 0f || !isFacingRight && moveX > 0f)
+        {
+            Vector2 localScale =  transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+       
+    }
+
+}
